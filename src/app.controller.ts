a91f581487c5fa8b26184d55f243a5e7dc5c94df
Gowtham <gowthamkumar1611@gmail.com>
2022-12-08 22:35:29 +0530

    code changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  getData(@Body() body){
+    console.log("data of received body",body)
+    this.appService.publishEvent(body)
+    return {"message":"data received"}
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('GREETING_SERVICE') private client: ClientProxy){}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async publishEvent(body) {
+    this.client.emit('book-created', body);
   }
 }
